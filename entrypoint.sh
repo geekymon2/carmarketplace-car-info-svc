c6824161ec8ca0262a100e1c50a0a0e727b6d63a
Geeky Mon2 <geekymon2@gmail.com>
2021-09-12 14:35:07 +1000

    added version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+source version.properties
 echo "This is the entrypoint script executing jar file: $ARTIFACT_NAME"
-echo "Image version: $VERSION"
+echo "Image version: $IMAGE_VERSION"
 java -jar "$ARTIFACT_NAME.jar"
