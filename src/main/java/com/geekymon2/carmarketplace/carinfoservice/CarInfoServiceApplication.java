e9319f3679643a597bd10d8674e4d279b3ac07e3
Geeky Mon2 <geekymon2@gmail.com>
2022-07-16 18:02:57 +1000

    added openapi

--- src/main/java/com/geekymon2/carmarketplace/carinfoservice/CarInfoServiceApplication.java
+++ src/main/java/com/geekymon2/carmarketplace/carinfoservice/CarInfoServiceApplication.java
@@ -1,5 +1,7 @@
 package com.geekymon2.carmarketplace.carinfoservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,6 +9,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Car Info Service API", version = "1.0", description = "Documentation Car Info Service API v1.0"))
 public class CarInfoServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/geekymon2/carmarketplace/carinfoservice/config/AppConfig.java
+++ src/main/java/com/geekymon2/carmarketplace/carinfoservice/config/AppConfig.java
@@ -1,25 +1,25 @@
 package com.geekymon2.carmarketplace.carinfoservice.config;
 
 import com.geekymon2.carmarketplace.carinfoservice.validation.CarInfoValidator;
-
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class AppConfig {
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI api() {
+        return new OpenAPI()
+            .info(new Info().title("Car Info Service")
+                .description("Car Info Service API")
+                .version("v0.0.1")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+            .externalDocs(new ExternalDocumentation()
+                .description("Car Info Service Documentation"));
     }
 
     @Bean
