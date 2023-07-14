cb8563795b8e893d9ae58fe9529c00756d365607
Geeky Mon2 <geekymon2@gmail.com>
2023-07-14 22:37:50 +1000

    update

--- src/main/java/com/geekymon2/carmarketplace/carinfoservice/CarInfoServiceApplication.java
+++ src/main/java/com/geekymon2/carmarketplace/carinfoservice/CarInfoServiceApplication.java
@@ -2,10 +2,8 @@ package com.geekymon2.carmarketplace.carinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@EnableEurekaClient
 @SpringBootApplication
 public class CarInfoServiceApplication {
 
