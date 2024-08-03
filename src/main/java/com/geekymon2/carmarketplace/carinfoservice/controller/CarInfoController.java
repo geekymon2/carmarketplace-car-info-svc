2ed6708e8ae331c9d6a28e1c884fc9f922f5e2b0
geekymon2 <geekymon2@gmail.com>
2024-08-03 14:41:30 +1000

    updated status API logic

--- src/main/java/com/geekymon2/carmarketplace/carinfoservice/controller/CarInfoController.java
+++ src/main/java/com/geekymon2/carmarketplace/carinfoservice/controller/CarInfoController.java
@@ -5,23 +5,18 @@ import com.geekymon2.carmarketplace.carinfoservice.entities.CarModel;
 import com.geekymon2.carmarketplace.carinfoservice.entities.CarModelType;
 import com.geekymon2.carmarketplace.carinfoservice.models.CarMakeDto;
 import com.geekymon2.carmarketplace.carinfoservice.models.CarModelDto;
-import com.geekymon2.carmarketplace.carinfoservice.models.StatusDto;
 import com.geekymon2.carmarketplace.carinfoservice.serviceimpl.CarInfoServiceImpl;
+import com.geekymon2.carmarketplace.core.common.ApiStatus;
+import com.geekymon2.carmarketplace.core.models.StatusDto;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.ws.rs.Path;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.nio.file.Paths;
 
 @Slf4j
 @RestController
@@ -39,26 +34,7 @@ public class CarInfoController {
     @Operation(summary = "Get the health status of this API", description = "Returns environment, API version, hostname and description")
     @GetMapping(value = "/status")
     public StatusDto getStatus() {
-        String hostname = "";
-        String environment = "";
-        String version = "0.0.0";
-        String UNKNOWN_LABEL = "unknown";
-
-        try {
-            hostname = java.net.InetAddress.getLocalHost().getHostName();
-            environment = System.getenv("ENVIRONMENT");
-            version = Files.readString(Paths.get("/version.properties")).split("=")[1];
-        }
-        catch (UnknownHostException uhx) {
-            hostname = UNKNOWN_LABEL;
-            log.error(String.format("Error getting hostname: %s", uhx));
-        }
-        catch (IOException iox) {
-            version = UNKNOWN_LABEL;
-            log.error(String.format("Error getting version: %s", iox));
-        }
-
-        return new StatusDto(environment, version, hostname, "This is the status endpoint");
+        return new ApiStatus().getStatus();
     }
 
     @Operation(summary = "Get all car makes from different manufactures", description = "Returns a list of car makes")
